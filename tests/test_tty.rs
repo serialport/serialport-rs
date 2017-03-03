6de74a9081bac9863014c5fd27b44f3830b171d6
Bryant Mairs <bryant@mai.rs>
2017-03-03 09:35:33 -0800

    Use assert_* when possible for better error messages

--- tests/test_tty.rs
+++ tests/test_tty.rs
@@ -18,21 +18,25 @@ fn test_ttyport_pair() {
             "Invalid file descriptor on master ptty");
     assert!(slave.as_raw_fd() > 0,
             "Invalid file descriptor on slae ptty");
-    assert!(master.as_raw_fd() != slave.as_raw_fd(),
-            "master and slave ptty's share the same file descriptor.");
+    assert_ne!(master.as_raw_fd(),
+               slave.as_raw_fd(),
+               "master and slave ptty's share the same file descriptor.");
 
     let msg = "Test Message";
     let mut buf = [0u8; 128];
 
     // Write the string on the master
-    assert!(master.write(msg.as_bytes()).unwrap() == msg.len(),
-            "Unable to write message on master.");
+    assert_eq!(master.write(msg.as_bytes()).unwrap(),
+               msg.len(),
+               "Unable to write message on master.");
 
     // Read it on the slave
     let nbytes = slave.read(&mut buf).expect("Unable to read bytes.");
-    assert!(nbytes == msg.len(),
-            "Read message length differs from sent message.");
+    assert_eq!(nbytes,
+               msg.len(),
+               "Read message length differs from sent message.");
 
-    assert!(str::from_utf8(&buf[..nbytes]).unwrap() == msg,
-            "Received message does not match sent");
+    assert_eq!(str::from_utf8(&buf[..nbytes]).unwrap(),
+               msg,
+               "Received message does not match sent");
 }
