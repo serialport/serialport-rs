5834e6d97adcec082443690d53c0e025a7ef033a
Bryant Mairs <bryant@mai.rs>
2018-02-13 21:13:03 -0800

    Make Mac tests in CI a little more robust
    The default timeout is 1ms, which leads to spurious failures
    on tests within the Travis CI Mac environment. Let's raise that
    to 10ms to see if it helps out any.

--- tests/test_tty.rs
+++ tests/test_tty.rs
@@ -3,9 +3,10 @@
 
 extern crate serialport;
 
-use std::os::unix::prelude::*;
 use std::io::{Read, Write};
+use std::os::unix::prelude::*;
 use std::str;
+use std::time::Duration;
 
 use serialport::{BaudRate, SerialPort};
 use serialport::posix::TTYPort;
@@ -13,6 +14,8 @@ use serialport::posix::TTYPort;
 #[test]
 fn test_ttyport_pair() {
     let (mut master, mut slave) = TTYPort::pair().expect("Unable to create ptty pair");
+    master.set_timeout(Duration::from_millis(10)).expect("Unable to set timeout on the master");
+    slave.set_timeout(Duration::from_millis(10)).expect("Unable to set timeout on the slave");
 
     // Test file descriptors.
     assert!(master.as_raw_fd() > 0,
@@ -27,9 +30,10 @@ fn test_ttyport_pair() {
     let mut buf = [0u8; 128];
 
     // Write the string on the master
-    assert_eq!(master.write(msg.as_bytes()).unwrap(),
+    let nbytes = master.write(msg.as_bytes()).expect("Unable to write bytes.");
+    assert_eq!(nbytes,
                msg.len(),
-               "Unable to write message on master.");
+               "Write message length differs from sent message.");
 
     // Read it on the slave
     let nbytes = slave.read(&mut buf).expect("Unable to read bytes.");
@@ -46,10 +50,10 @@ fn test_ttyport_pair() {
 fn test_ttyport_timeout() {
     let result = std::sync::Arc::new(std::sync::Mutex::new(None));
     let result_thread = result.clone();
-    
+
     std::thread::spawn(move || {
         let (mut master, _slave) = TTYPort::pair().expect("Unable to create ptty pair");
-        master.set_timeout(std::time::Duration::new(1, 0)).unwrap();
+        master.set_timeout(Duration::new(1, 0)).unwrap();
 
         let mut buffer = [0u8];
         let read_res = master.read(&mut buffer);
