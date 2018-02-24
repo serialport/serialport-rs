addb8509ad94e0b8c44e17995002553203cc5910
Bryant Mairs <bryant@mai.rs>
2018-02-24 09:34:36 -0800

    Refactor tests for try_clone
    The try clone tests were failing on some platforms, so simplify
    them a bit. Primarily remove unnecessary threads.

--- tests/test_try_clone.rs
+++ tests/test_try_clone.rs
@@ -1,61 +1,56 @@
 #![cfg(unix)]
 extern crate serialport;
 
-use std::io::{Read,Write,ErrorKind};
+use std::io::{Read, Write};
 use serialport::posix::TTYPort;
 use serialport::SerialPort;
 
+// Test that cloning a port works as expected
 #[test]
 fn test_try_clone() {
-    use std::{mem, thread};
+    let (master, mut slave) = TTYPort::pair().expect("Unable to create ptty pair");
 
-    let (mut master, mut slave) = TTYPort::pair().expect("Unable to create ptty pair");
-
-    // Slave tty is just used to forward what the master tty sends back to it.
-    // So we can clone master tty and test that we can simultaneously read and write
-    // to it and check that the values are correct.
-    let loopback = thread::spawn(move || {
-        let mut buffer: [u8; 10] = unsafe { mem::uninitialized() };
-
-        // loops until the serial tty is closed
-        loop {
-            match slave.read(&mut buffer) {
-                Ok(bytes) => if bytes > 0 {slave.write(&buffer[0..bytes]).unwrap();} else { break; },
-                Err(ref e)  => {assert_eq!(e.kind(),ErrorKind::ConnectionAborted);},
-            }
-        }
-    });
-
-    // Create clone in an inner scope to test that dropping a clone do not
-    // shuts off the serial device.
+    // Create the clone in an inner scope to test that dropping a clone doesn't close the original
+    // port
     {
         let mut clone = master.try_clone().expect("Failed to clone");
-        let writes = thread::spawn(move || {
-            let bytes = [b'a', b'b', b'c', b'd', b'e', b'f'];
-            clone.write(&bytes).unwrap();
-        });
+        let bytes = [b'a', b'b', b'c', b'd', b'e', b'f'];
+        clone.write(&bytes).unwrap();
         let mut buffer = [0; 6];
-        master.read_exact(&mut buffer).unwrap();
-        writes.join().unwrap();
+        slave.read_exact(&mut buffer).unwrap();
         assert_eq!(buffer, [b'a', b'b', b'c', b'd', b'e', b'f']);
     }
 
     // Second try to check that the serial device is not closed
     {
         let mut clone = master.try_clone().expect("Failed to clone");
-        let writes = thread::spawn(move || {
-            let bytes = [b'g', b'h', b'i', b'j', b'k', b'l'];
-            clone.write(&bytes).unwrap();
-        });
+        let bytes = [b'g', b'h', b'i', b'j', b'k', b'l'];
+        clone.write(&bytes).unwrap();
         let mut buffer = [0; 6];
-        master.read_exact(&mut buffer).unwrap();
-        writes.join().unwrap();
+        slave.read_exact(&mut buffer).unwrap();
         assert_eq!(buffer, [b'g', b'h', b'i', b'j', b'k', b'l']);
     }
+}
 
-    // Check that once all reference to the master tty are gone, the
-    // serial device is closed and the loopback thread should finish
-    mem::drop(master);
+// Test moving a cloned port into a thread
+#[test]
+fn test_try_clone_move() {
+    use std::thread;
+
+    let (master, mut slave) = TTYPort::pair().expect("Unable to create ptty pair");
+
+    let mut clone = master.try_clone().expect("Failed to clone the slave");
+    let loopback = thread::spawn(move || {
+        let bytes = [b'a', b'b', b'c', b'd', b'e', b'f'];
+        clone.write(&bytes).unwrap();
+    });
+
+    // Create clone in an inner scope to test that dropping a clone does not destroy the serialport
+    let mut buffer = [0; 6];
+    slave.read_exact(&mut buffer).unwrap();
+    assert_eq!(buffer, [b'a', b'b', b'c', b'd', b'e', b'f']);
+
+    // The thread should have already ended, but we'll make sure here anyways.
     loopback.join().unwrap();
 }
 
