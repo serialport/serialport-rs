2b11b0559dfc9eb8b974834e31754408ed07aea5
Bryant Mairs <bryant@mai.rs>
2018-05-28 17:18:16 -0700

    Ignore arbitrary baud rate tests on ios/macos
    This functionality hasn't been merged yet (see !30), so testing it
    now always results in failure. Instead disable it for now.

--- tests/test_tty.rs
+++ tests/test_tty.rs
@@ -91,6 +91,8 @@ fn test_ttyport_set_standard_baud() {
 }
 
 #[test]
+// Until !30 is merged mac and ios don't support arbitrary baud rates so don't run
+#[cfg_attr(any(target_os = "ios", target_os = "macos"), ignore)]
 fn test_ttyport_set_nonstandard_baud() {
     // `master` must be used here as Dropping it causes slave to be deleted by the OS.
     // TODO: Convert this to a statement-level attribute once
