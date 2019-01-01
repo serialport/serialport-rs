86b2daa89c5053022b85afbd499e55925048f8bc
Bryant Mairs <bryant@mai.rs>
2019-01-01 11:00:51 +0000

    Support port enumeration on FreeBSD

--- src/posix/tty.rs
+++ src/posix/tty.rs
@@ -1151,6 +1151,28 @@ cfg_if! {
                 "Serial port enumeration disabled (to enable compile with the 'libudev' feature)",
             ))
         }
+    } else if #[cfg(target_os = "freebsd")] {
+        /// Scans the system for serial ports and returns a list of them.
+        /// The `SerialPortInfo` struct contains the name of the port
+        /// which can be used for opening it.
+        pub fn available_ports() -> ::Result<Vec<SerialPortInfo>> {
+            let mut vec = Vec::new();
+            let dev_path = Path::new("/dev/");
+            for path in dev_path.read_dir()? {
+                let path = path?;
+                let filename = path.file_name();
+                let filename_string = filename.to_string_lossy();
+                if filename_string.starts_with("cuaU") || filename_string.starts_with("cuau") || filename_string.starts_with("cuad") {
+                    if !filename_string.ends_with(".init") && !filename_string.ends_with(".lock") {
+                        vec.push(SerialPortInfo {
+                            port_name: path.path().to_string_lossy().to_string(),
+                            port_type: ::SerialPortType::Unknown,
+                        });
+                    }
+                }
+            }
+            Ok(vec)
+        }
     } else {
         /// Enumerating serial ports on this platform is not supported
         pub fn available_ports() -> ::Result<Vec<SerialPortInfo>> {
