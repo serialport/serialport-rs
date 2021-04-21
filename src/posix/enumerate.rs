7816e2823c3e56a0552b0d4fb2b6275e27430316
Anatol Ulrich <anatol.ulrich@ferrous-systems.com>
2021-04-21 17:17:25 +0200

    remove import

--- src/posix/enumerate.rs
+++ src/posix/enumerate.rs
@@ -1,7 +1,5 @@
 #[cfg(any(target_os = "ios", target_os = "macos"))]
 use nix::libc::{c_char, c_void};
-#[cfg(any(target_os = "ios", target_os = "macos"))]
-use os_version::OsVersion;
 #[cfg(all(target_os = "linux", not(target_env = "musl"), feature = "libudev"))]
 use std::ffi::OsStr;
 #[cfg(any(target_os = "ios", target_os = "macos"))]
