904ddb35d3f45cfffa71379d995663eb7428b587
Markus Kohlhase <markus.kohlhase@slowtec.de>
2021-11-18 15:48:51 +0100

    Update nix dependency to v0.23.x

--- src/posix/error.rs
+++ src/posix/error.rs
@@ -5,56 +5,35 @@ use crate::{Error, ErrorKind};
 #[cfg(all(target_os = "linux", not(target_env = "musl"), feature = "libudev"))]
 impl From<libudev::Error> for Error {
     fn from(e: libudev::Error) -> Error {
-        let description = e.description().to_string();
-        match e.kind() {
-            libudev::ErrorKind::NoMem => Error::new(ErrorKind::Unknown, description),
-            libudev::ErrorKind::InvalidInput => Error::new(ErrorKind::InvalidInput, description),
-            libudev::ErrorKind::Io(a) => Error::new(ErrorKind::Io(a), description),
-        }
+        use libudev::ErrorKind as K;
+        let kind = match e.kind() {
+            K::NoMem => ErrorKind::Unknown,
+            K::InvalidInput => ErrorKind::InvalidInput,
+            K::Io(a) => ErrorKind::Io(a),
+        };
+        Error::new(kind, e.description())
     }
 }
 
 impl From<nix::Error> for Error {
     fn from(e: nix::Error) -> Error {
-        match e {
-            nix::Error::InvalidPath | nix::Error::InvalidUtf8 => {
-                Error::new(ErrorKind::InvalidInput, "Invalid input")
-            }
-            nix::Error::UnsupportedOperation => Error::new(ErrorKind::Unknown, "Unknown error"),
-            nix::Error::Sys(e @ nix::errno::Errno::ETIMEDOUT) => {
-                Error::new(ErrorKind::Io(io::ErrorKind::TimedOut), e.desc())
-            }
-            nix::Error::Sys(e @ nix::errno::Errno::ECONNABORTED) => {
-                Error::new(ErrorKind::Io(io::ErrorKind::ConnectionAborted), e.desc())
-            }
-            nix::Error::Sys(e @ nix::errno::Errno::ECONNRESET) => {
-                Error::new(ErrorKind::Io(io::ErrorKind::ConnectionReset), e.desc())
-            }
-            nix::Error::Sys(e @ nix::errno::Errno::ECONNREFUSED) => {
-                Error::new(ErrorKind::Io(io::ErrorKind::ConnectionRefused), e.desc())
-            }
-            nix::Error::Sys(e @ nix::errno::Errno::ENOTCONN) => {
-                Error::new(ErrorKind::Io(io::ErrorKind::NotConnected), e.desc())
-            }
-            nix::Error::Sys(e @ nix::errno::Errno::EADDRINUSE) => {
-                Error::new(ErrorKind::Io(io::ErrorKind::AddrInUse), e.desc())
-            }
-            nix::Error::Sys(e @ nix::errno::Errno::EADDRNOTAVAIL) => {
-                Error::new(ErrorKind::Io(io::ErrorKind::AddrNotAvailable), e.desc())
-            }
-            nix::Error::Sys(e @ nix::errno::Errno::EAGAIN) => {
-                Error::new(ErrorKind::Io(io::ErrorKind::WouldBlock), e.desc())
-            }
-            nix::Error::Sys(e @ nix::errno::Errno::EINTR) => {
-                Error::new(ErrorKind::Io(io::ErrorKind::Interrupted), e.desc())
-            }
-            nix::Error::Sys(e @ nix::errno::Errno::EACCES) => {
-                Error::new(ErrorKind::Io(io::ErrorKind::PermissionDenied), e.desc())
-            }
-            nix::Error::Sys(e @ nix::errno::Errno::ENOENT) => {
-                Error::new(ErrorKind::Io(io::ErrorKind::NotFound), e.desc())
-            }
-            nix::Error::Sys(e) => Error::new(ErrorKind::Unknown, e.desc()),
-        }
+        use io::ErrorKind as IO;
+        use nix::errno::Errno as E;
+        use ErrorKind as K;
+        let kind = match e {
+            E::ETIMEDOUT => K::Io(IO::TimedOut),
+            E::ECONNABORTED => K::Io(IO::ConnectionAborted),
+            E::ECONNRESET => K::Io(IO::ConnectionReset),
+            E::ECONNREFUSED => K::Io(IO::ConnectionRefused),
+            E::ENOTCONN => K::Io(IO::NotConnected),
+            E::EADDRINUSE => K::Io(IO::AddrInUse),
+            E::EADDRNOTAVAIL => K::Io(IO::AddrNotAvailable),
+            E::EAGAIN => K::Io(IO::WouldBlock),
+            E::EINTR => K::Io(IO::Interrupted),
+            E::EACCES => K::Io(IO::PermissionDenied),
+            E::ENOENT => K::Io(IO::NotFound),
+            _ => K::Unknown,
+        };
+        Error::new(kind, e.desc())
     }
 }
