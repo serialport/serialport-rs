57985f9b306aa46c073b61ce1a832cefcc2d69eb
Danilo Bargen <mail@dbrgn.ch>
2022-11-13 17:53:01 +0100

    embedded: Implement both blocking and non-blocking traits

--- src/embedded.rs
+++ src/embedded.rs
@@ -4,7 +4,7 @@
 
 use std::io;
 
-use embedded_hal_nb::serial;
+use embedded_hal::serial::{ErrorType, ErrorKind};
 
 use crate::SerialPort;
 
@@ -13,47 +13,75 @@ pub struct SerialError {
     kind: io::ErrorKind,
 }
 
-// Implement `serial::Error` for SerialError
-impl serial::Error for SerialError {
-    fn kind(&self) -> serial::ErrorKind {
+impl embedded_hal::serial::Error for SerialError {
+    fn kind(&self) -> ErrorKind {
         #[allow(clippy::match_single_binding)]
         match self.kind {
-            _other => serial::ErrorKind::Other,
+            _ => ErrorKind::Other,
         }
     }
 }
 
-fn io_error_to_nb(err: io::Error) -> nb::Error<SerialError> {
-    match err.kind() {
-        io::ErrorKind::WouldBlock | io::ErrorKind::Interrupted => nb::Error::WouldBlock,
-        other => nb::Error::Other(SerialError { kind: other }),
+impl From<io::Error> for SerialError {
+    fn from(e: io::Error) -> Self {
+        SerialError {
+            kind: e.kind(),
+        }
     }
 }
 
-impl serial::ErrorType for Box<dyn SerialPort> {
+impl ErrorType for Box<dyn SerialPort> {
     type Error = SerialError;
 }
 
-impl serial::Read<u8> for Box<dyn SerialPort> {
-    fn read(&mut self) -> nb::Result<u8, Self::Error> {
-        let mut buffer = [0; 1];
-        let bytes_read = io::Read::read(self, &mut buffer).map_err(io_error_to_nb)?;
-        if bytes_read > 0 {
-            Ok(buffer[0])
-        } else {
-            Err(nb::Error::WouldBlock)
+
+mod nonblocking {
+    use super::*;
+    use embedded_hal_nb::serial;
+
+    fn io_error_to_nb(err: io::Error) -> nb::Error<SerialError> {
+        match err.kind() {
+            io::ErrorKind::WouldBlock | io::ErrorKind::Interrupted => nb::Error::WouldBlock,
+            other => nb::Error::Other(SerialError { kind: other }),
+        }
+    }
+
+    impl serial::Read<u8> for Box<dyn SerialPort> {
+        fn read(&mut self) -> nb::Result<u8, Self::Error> {
+            let mut buffer = [0; 1];
+            let bytes_read = io::Read::read(self, &mut buffer).map_err(io_error_to_nb)?;
+            if bytes_read > 0 {
+                Ok(buffer[0])
+            } else {
+                Err(nb::Error::WouldBlock)
+            }
+        }
+    }
+
+    impl serial::Write<u8> for Box<dyn SerialPort> {
+        fn write(&mut self, word: u8) -> nb::Result<(), Self::Error> {
+            io::Write::write(self, &[word])
+                .map_err(io_error_to_nb)
+                .map(|_| ())
+        }
+
+        fn flush(&mut self) -> nb::Result<(), Self::Error> {
+            io::Write::flush(self).map_err(io_error_to_nb)
         }
     }
 }
 
-impl serial::Write<u8> for Box<dyn SerialPort> {
-    fn write(&mut self, word: u8) -> nb::Result<(), Self::Error> {
-        io::Write::write(self, &[word])
-            .map_err(io_error_to_nb)
-            .map(|_| ())
-    }
+mod blocking {
+    use super::*;
+    use embedded_hal::serial;
 
-    fn flush(&mut self) -> nb::Result<(), Self::Error> {
-        io::Write::flush(self).map_err(io_error_to_nb)
+    impl serial::Write<u8> for Box<dyn SerialPort> {
+        fn write(&mut self, buffer: &[u8]) -> Result<(), Self::Error> {
+            Ok(io::Write::write_all(self, buffer)?)
+        }
+
+        fn flush(&mut self) -> Result<(), Self::Error> {
+            Ok(io::Write::flush(self)?)
+        }
     }
 }
