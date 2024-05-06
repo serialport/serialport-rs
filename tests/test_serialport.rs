b8a8d05bf84de711f60aecc8b1c5e7ec31157fa0
Christian Meusel <christian.meusel@posteo.de>
2024-05-06 18:42:33 +0200

    Check test configuration at runtime and on demand
    This cleans up the initial attempt of specifying the hardware ports to
    be used for testing by:
    
        * Requiring the configuration environment variables only to be
          present when actually required by a test case
    
        * Evaluating these environment variables at runtime, eliminating the
          footgun that changing them does not trigger rebuilding the tests
    
        * No longer requiring to set dummy configuration variables when not
          running any hardware tests at all (like in CI).
    
    The configuration environment variable prefix changes from
    SERIALPORT_RS_TEST_ to SERIALPORT_TEST_ to match the crate name and not
    the repository name.
    
    The changes add two new dev dependencies which both look worth it:
    
        * The envconfig crate eliminates any need for custom error reporting
          about which environment variable is missing (which not comes for
          free when just using std::env::var)
    
        * The rstest crate conveniently allows to use test fixtures and
          supports parametrizing tests which will likely be used by the next
          test cases to come

--- tests/test_serialport.rs
+++ tests/test_serialport.rs
@@ -1,13 +1,14 @@
 //! Tests for the `SerialPort` trait.
 #![cfg(unix)]
 
+mod config;
+
+use config::{hw_config, HardwareConfig};
+use rstest::rstest;
 use serialport::*;
 use std::time::Duration;
 
-const PORT_1: &str = env!("SERIALPORT_RS_TEST_PORT_1");
-const PORT_2: &str = env!("SERIALPORT_RS_TEST_PORT_2");
-
-#[test]
+#[rstest]
 #[cfg_attr(feature = "ignore-hardware-tests", ignore)]
 fn test_listing_ports() {
     let ports = serialport::available_ports().expect("No ports found!");
@@ -16,32 +17,34 @@ fn test_listing_ports() {
     }
 }
 
-#[test]
+#[rstest]
 #[cfg_attr(feature = "ignore-hardware-tests", ignore)]
-fn test_opening_found_ports() {
+fn test_opening_found_ports(hw_config: HardwareConfig) {
     // There is no guarantee that we even might open the ports returned by `available_ports`. But
     // the ports we are using for testing shall be among them.
     let ports = serialport::available_ports().unwrap();
-    assert!(ports.iter().any(|info| info.port_name == PORT_1));
-    assert!(ports.iter().any(|info| info.port_name == PORT_2));
+    assert!(ports.iter().any(|info| info.port_name == hw_config.port_1));
+    assert!(ports.iter().any(|info| info.port_name == hw_config.port_2));
 }
 
-#[test]
+#[rstest]
 #[cfg_attr(feature = "ignore-hardware-tests", ignore)]
-fn test_opening_port() {
-    serialport::new(PORT_1, 9600).open().unwrap();
+fn test_opening_port(hw_config: HardwareConfig) {
+    serialport::new(hw_config.port_1, 9600).open().unwrap();
 }
 
-#[test]
+#[rstest]
 #[cfg_attr(feature = "ignore-hardware-tests", ignore)]
-fn test_opening_native_port() {
-    serialport::new(PORT_1, 9600).open_native().unwrap();
+fn test_opening_native_port(hw_config: HardwareConfig) {
+    serialport::new(hw_config.port_1, 9600)
+        .open_native()
+        .unwrap();
 }
 
-#[test]
+#[rstest]
 #[cfg_attr(feature = "ignore-hardware-tests", ignore)]
-fn test_configuring_ports() {
-    serialport::new(PORT_1, 9600)
+fn test_configuring_ports(hw_config: HardwareConfig) {
+    serialport::new(hw_config.port_1, 9600)
         .data_bits(DataBits::Five)
         .flow_control(FlowControl::None)
         .parity(Parity::None)
@@ -51,17 +54,20 @@ fn test_configuring_ports() {
         .unwrap();
 }
 
-#[test]
+#[rstest]
 #[cfg_attr(feature = "ignore-hardware-tests", ignore)]
-fn test_duplicating_port_config() {
-    let port1_config = serialport::new(PORT_1, 9600)
+fn test_duplicating_port_config(hw_config: HardwareConfig) {
+    let port1_config = serialport::new(hw_config.port_1, 9600)
         .data_bits(DataBits::Five)
         .flow_control(FlowControl::None)
         .parity(Parity::None)
         .stop_bits(StopBits::One)
         .timeout(Duration::from_millis(1));
 
-    let port2_config = port1_config.clone().path(PORT_2).baud_rate(115_200);
+    let port2_config = port1_config
+        .clone()
+        .path(hw_config.port_2)
+        .baud_rate(115_200);
 
     let _port1 = port1_config.open().unwrap();
     let _port1 = port2_config.open().unwrap();
