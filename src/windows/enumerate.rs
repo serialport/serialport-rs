0720d5a64109d43b2d5fa5fb170e3ed052dbf9b1
Christian Meusel <christian.meusel@posteo.de>
2024-05-12 11:25:05 +0200

    Stick to filtering out LPT ports on Windows
    According to issue #187 null-modem ports are appearing as CNCxy and
    there have not been complaints about other devices showing up
    unintendedly so far.

--- src/windows/enumerate.rs
+++ src/windows/enumerate.rs
@@ -459,7 +459,7 @@ pub fn available_ports() -> Result<Vec<SerialPortInfo>> {
             );
 
             // This technique also returns parallel ports, so we filter these out.
-            if !port_name.starts_with("COM") {
+            if port_name.starts_with("LPT") {
                 continue;
             }
 
