8ed97a28ba0c352938e1bee6c6df28163177cdd6
Casper Meijn <casper@meijn.net>
2025-05-09 08:46:47 +0200

    feat: Add integration with schemars
    Integrate with `schemars`, which allows to generate a JSON schema for a `serde` generated JSON file.
    
    A v1 of `schemars` is in the works, but that will take some months. It seems the derive API will not change.

--- src/lib.rs
+++ src/lib.rs
@@ -140,6 +140,7 @@ impl From<Error> for io::Error {
 /// Number of bits per character
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 pub enum DataBits {
     /// 5 bits per character
     Five,
@@ -201,6 +202,7 @@ impl TryFrom<u8> for DataBits {
 /// transmitted.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 pub enum Parity {
     /// No parity bit.
     None,
@@ -227,6 +229,7 @@ impl fmt::Display for Parity {
 /// Stop bits are transmitted after every character.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 pub enum StopBits {
     /// One stop bit.
     One,
@@ -268,6 +271,7 @@ impl TryFrom<u8> for StopBits {
 /// Flow control modes
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 pub enum FlowControl {
     /// No flow control.
     None,
@@ -307,6 +311,7 @@ impl FromStr for FlowControl {
 /// [`clear`]: trait.SerialPort.html#tymethod.clear
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 pub enum ClearBuffer {
     /// Specify to clear data received but not read
     Input,
@@ -815,6 +820,7 @@ impl fmt::Debug for dyn SerialPort {
 /// Contains all possible USB information about a `SerialPort`
 #[derive(Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 pub struct UsbPortInfo {
     /// Vendor ID
     pub vid: u16,
@@ -836,6 +842,7 @@ pub struct UsbPortInfo {
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 /// Identify where a particular USB device is on the system.
 pub struct Location {
     bus_id: String,
@@ -985,6 +992,7 @@ impl std::fmt::Debug for UsbPortInfo {
 /// The physical type of a `SerialPort`
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 pub enum SerialPortType {
     /// The serial port is connected via USB
     UsbPort(UsbPortInfo),
@@ -999,6 +1007,7 @@ pub enum SerialPortType {
 /// A device-independent implementation of serial port information
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 pub struct SerialPortInfo {
     /// The short name of the serial port
     pub port_name: String,
