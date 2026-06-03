faecee43e0855f54fb0cf4422c4a1b4ac3c8115e
Tanmay <nnon92333@gmail.com>
2026-06-03 11:59:39 +0530

    Implement SerialPortExt and unify POSIX structs

--- src/posix/mod.rs
+++ src/posix/mod.rs
@@ -8,3 +8,49 @@ mod ioctl;
 mod poll;
 mod termios;
 mod tty;
+
+use std::os::unix::io::{AsRawFd, FromRawFd, IntoRawFd, RawFd};
+
+#[allow(missing_docs)]
+pub trait SerialPortExt {
+    fn exclusive(&self) -> bool;
+    fn set_exclusive(&mut self, exclusive: bool) -> crate::Result<()>;
+    fn send_break(&self, duration: BreakDuration) -> crate::Result<()>;
+    fn pair() -> crate::Result<(crate::SerialPort, crate::SerialPort)>;
+}
+
+impl SerialPortExt for crate::SerialPort {
+    fn exclusive(&self) -> bool {
+        self.0.exclusive()
+    }
+
+    fn set_exclusive(&mut self, exclusive: bool) -> crate::Result<()> {
+        self.0.set_exclusive(exclusive)
+    }
+
+    fn send_break(&self, duration: BreakDuration) -> crate::Result<()> {
+        self.0.send_break(duration)
+    }
+
+    fn pair() -> crate::Result<(crate::SerialPort, crate::SerialPort)> {
+        TTYPort::pair().map(|(m, s)| (crate::SerialPort(m), crate::SerialPort(s)))
+    }
+}
+
+impl AsRawFd for crate::SerialPort {
+    fn as_raw_fd(&self) -> RawFd {
+        self.0.as_raw_fd()
+    }
+}
+
+impl IntoRawFd for crate::SerialPort {
+    fn into_raw_fd(self) -> RawFd {
+        self.0.into_raw_fd()
+    }
+}
+
+impl FromRawFd for crate::SerialPort {
+    unsafe fn from_raw_fd(fd: RawFd) -> Self {
+        crate::SerialPort(crate::posix::TTYPort::from_raw_fd(fd))
+    }
+}
