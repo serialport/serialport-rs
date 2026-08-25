7d4ed44bca83fcb379786b27d2172380905308d9
Christian Meusel <christian.meusel@posteo.de>
2026-08-25 08:08:22 +0200

    Accept dash in serial numbers on Windows
    This fixes the specific case from
    https://github.com/serialport/serialport-rs/issues/351. But it looks
    like there is more to consider to get serial number parsing right.

--- src/windows/enumerate.rs
+++ src/windows/enumerate.rs
@@ -17,7 +17,7 @@ use windows_sys::Win32::System::Registry::{
 
 use crate::{Error, ErrorKind, Result, SerialPortInfo, SerialPortType, UsbPortInfo};
 
-const CONNECTOR_PUNCTUATION_SELECTION: &[char] = &[':', '_', '\u{ff3f}'];
+const CONNECTOR_PUNCTUATION_SELECTION: &[char] = &['-', ':', '_', '\u{ff3f}'];
 
 /// takes normal Rust `str` and outputs a null terminated UTF-16 encoded string
 fn as_utf16(utf8: &str) -> Vec<u16> {
